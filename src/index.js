d130725a75859ab2e36ab81a48f12af78fc01441
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-05-08 18:50:26 +0400

    feat: add to textarea

--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import { keyboard } from "./keyboard";
 const head = window.document.querySelector("head");
 
 function includeCSS() {
-    const style = window.document.createElement("link");
-    style.href = "/dist/style.css";
-    style.rel = "stylesheet";
-    head.append(style);
+  const style = window.document.createElement("link");
+  style.href = "/dist/style.css";
+  style.rel = "stylesheet";
+  head.append(style);
 }
 includeCSS();
-
-const renderTextArea = () => {
-    const textArea = document.createElement("div");
-    textArea.classList.add("input");
-    textArea.innerHTML = "<textarea class=\"input_textarea\" id=\"textarea\" ></textarea>";
-    // console.log(textArea);
-    window.document.querySelector("body").append(textArea);
-};
-renderTextArea();
--- src/keyboard.js
+++ src/keyboard.js
@@ -1,10 +1,9 @@
 // import { Keys } from "./Keys";
 console.log("Running");
-
 document.querySelector("body").innerHTML =
   '<div class="keyboard" id="keyboard"> </div>';
 const keys = [
-  96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8, 9, 81, 87, 69, 82,
+  192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8, 9, 81, 87, 69, 82,
   84, 89, 85, 73, 79, 80, 219, 221, 220, 20, 65, 83, 68, 70, 71, 72, 74, 75, 76,
   186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38, 16, 17, 91,
   18, 32, 18, 17, 18, 37, 40, 39,
@@ -76,6 +75,17 @@ const keys2 = [
 ];
 const keyArr = [];
 let count = 0;
+const renderTextArea = () => {
+  const textArea = document.createElement("div");
+  textArea.classList.add("input");
+  textArea.innerHTML =
+    '<textarea class="input_textarea" id="textarea" ></textarea>';
+  // console.log(textArea);
+  window.document.querySelector("body").append(textArea);
+};
+renderTextArea();
+const textArea = document.querySelector(".input_textarea");
+console.log(textArea);
 
 const createLine = () => {
   const line = document.createElement("div");
@@ -91,15 +101,16 @@ const renderKeys = () => {
     temp3 = "",
     temp4 = "";
   for (let i = 0; i < keys2.length; i++) {
-    if (i < 14) temp += `<div id="${keys2[i]}" class="key">${keys2[i]}</div>`;
+    if (i < 14)
+      temp += `<div id="${keys2[i]}" class="key" data ="${keys[i]}">${keys2[i]}</div>`;
     else if ((i > 13) & (i <= 27))
-      temp1 += `<div id="${keys2[i]}" class="key">${keys2[i]}</div>`;
+      temp1 += `<div id="${keys2[i]}" class="key" data ="${keys[i]}">${keys2[i]}</div>`;
     else if ((i > 27) & (i < 41))
-      temp2 += `<div id="${keys2[i]}" class="key">${keys2[i]}</div>`;
+      temp2 += `<div id="${keys2[i]}" class="key" data ="${keys[i]}">${keys2[i]}</div>`;
     else if ((i >= 41) & (i < 54))
-      temp3 += `<div id="${keys2[i]}" class="key">${keys2[i]}</div>`;
+      temp3 += `<div id="${keys2[i]}" class="key" data ="${keys[i]}">${keys2[i]}</div>`;
     else if (i >= 54)
-      temp4 += `<div id="${keys2[i]}" class="key">${keys2[i]}</div>`;
+      temp4 += `<div id="${keys2[i]}" class="key" data ="${keys[i]}">${keys2[i]}</div>`;
   }
   for (let j = 0; j < 5; j++) {
     createLine();
@@ -113,19 +124,45 @@ const renderKeys = () => {
   document.querySelector("#line_4").innerHTML = temp4;
 };
 renderKeys();
+let keyButton = document.querySelectorAll(".key");
+console.log(keyButton);
 
 document.onkeydown = function (e) {
-  let t = [];
-  //   keys.push(e);
-  t.push(e.key);
-  console.log(t);
+  document.querySelector(`[data="${e.keyCode}"]`).classList.add("active");
+  console.log(e.key);
+  textArea.insertAdjacentHTML("beforeend", `${e.key}`);
 };
+document.onkeyup = function (e) {
+  keyButton.forEach((el) => el.classList.remove("active"));
+};
+// keyButton.forEach((el) =>
+//   el.addEventListener("keydown", (e) =>
+//     textArea.insertAdjacentHTML("beforeend", `${el.textContent}`)
+//   )
+// );
+keyButton.forEach((el) => {
+  el.onclick = (e) => {
+    keyButton.forEach((element) => element.classList.remove("active"));
+    let data = el.getAttribute("data");
+    document.querySelector(`[data = "${data}"]`).classList.add("active");
+    console.log(el.textContent);
+    textArea.insertAdjacentHTML("beforeend", `${el.textContent}`);
+  };
+});
+// document.addEventListener("mouseup", (e) => {
+//   keyButton.forEach((el) => {
+//     el.classList.remove("active");
+//     console.log(el);
+//   });
+// });
 
-const special = document.querySelectorAll("div.key");
-// let specialArr = arrayFrom.special;
-for (let spec of special) {
-  if (spec.innerText.length > 1) {
-    spec.className += " special";
+const printKeys = () => {};
+
+const addClassNameToSpecial = () => {
+  for (let spec of keyButton) {
+    if (spec.innerText.length > 1) {
+      spec.className += " special";
+    }
   }
-  // console.log(spec.innerHTML.length);
-}
+};
+addClassNameToSpecial();
